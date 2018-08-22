3af72401923a85ac0eaf707016c9c3767a3b7331
W-B-S <lws99927800@163.com>
2018-08-22 22:32:07 +0800

    smart contract demo

--- chapter-5/functionSelector.go
+++ chapter-5/functionSelector.go
@@ -6,12 +6,14 @@ import (
 )
 
 func FunctionSelector() {
-	a := []byte("baz(uint32,bool)")
-	fmt.Printf("\nsha3(%s)\t\t\t\t\t\t=    %x", a, crypto.Keccak256(a))
-	b := []byte("bar(bytes3[2])")
-	fmt.Printf("\nsha3(%s)\t\t\t\t\t\t=    %x", b, crypto.Keccak256(b))
-	c := []byte("sam(bytes,bool,uint256[])")
-	fmt.Printf("\nsha3(%s)\t\t\t\t=    %x", c, crypto.Keccak256(c))
-	d := []byte("f(uint256,uint32[],bytes10,bytes)")
-	fmt.Printf("\nsha3(%s)\t\t=    %x", d, crypto.Keccak256(d))
+	a := []byte("complexDynamic(uint256[][],string[])")
+	fmt.Printf("\nsha3(%s)\t=\t%x", a, crypto.Keccak256(a))
+	b := []byte("dynamicTuple(uint256,uint32[],bytes10,bytes)")
+	fmt.Printf("\nsha3(%s)\t=\t%x", b, crypto.Keccak256(b))
+	c := []byte("simpleDynamic(bytes,bool,uint256[])")
+	fmt.Printf("\nsha3(%s)\t=\t%x", c, crypto.Keccak256(c))
+	d := []byte("simpleStatic(uint32,bool)")
+	fmt.Printf("\nsha3(%s)\t=\t%x", d, crypto.Keccak256(d))
+	e := []byte("staticArray(bytes3[2])")
+	fmt.Printf("\nsha3(%s)\t=\t%x", e, crypto.Keccak256(e))
 }
